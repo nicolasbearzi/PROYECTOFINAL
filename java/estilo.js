b290399bf0e2e60c60e15d433b6810c2373744c4
nicolasbearzi <89425130+nicolasbearzi@users.noreply.github.com>
2021-11-15 12:42:09 -0300

    Add files via upload

---
+++ java/estilo.js
@@ -0,0 +1,12 @@
+let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+typewriter
+  .pauseFor(2500)
+  .typeString('3 De Febrero')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
